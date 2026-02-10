8765d558c8d743c0b0fffbc4126fe00f0efac4d2
David Henry <davidhy2016@gmail.com>
2026-02-10 20:48:59 -0300

    testes malucos

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+
+import { App } from './App';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
+    <div>
+      <h1>Sei lá (do main)</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci
+        placeat officia illum, illo voluptates eaque repellendus perspiciatis
+        corporis quam atque ducimus maxime iusto omnis voluptatibus tenetur
+        corrupti autem aperiam cumque!
+      </p>
+    </div>
   </StrictMode>,
-)
+);
