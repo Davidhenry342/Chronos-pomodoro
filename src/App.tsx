ab367bdb0cec5182c4bb2a517335e487fc8aea42
David Henry Peres Viana <davidhy2016@gmail.com>
2026-08-08 12:07:11 -0300

    Foi tanta modificação que eu nem lembro

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
 import { Home } from './pages/Home/Index';
-
 import './styles/theme.css';
 import './styles/global.css';
+// import { NotFound } from './pages/NotFound/Index';
+// import { AboutPomodoro } from './pages/AboutPomodoro/Index';
+import { TaskContextProvider } from './contexts/TaskContext/TaskContextProvider';
 
 export function App() {
-  return <Home />;
+  return (
+    <TaskContextProvider>
+      <Home />
+    </TaskContextProvider>
+  );
 }
